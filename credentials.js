32c755a148c8cf9cd4305724d101b3c9749abc70
Benyam Ephrem <ephremdsg@gmail.com>
2018-04-07 00:39:04 -0400

    As my code becomes more rushed my commit messages get worse and worse. Db fixed.

--- credentials.js
+++ credentials.js
@@ -1,4 +1,4 @@
 module.exports = {
     SESSION_ID_SECRET: 'VX^#o*2fp%O$7D#2_e+F', //Secret used to sign the session ID cookie
-    MONGODB_CONNECTION_URI: "mongodb+srv://admin:Q04QR1pUPd1hhzxy@impulse-0bqxp.mongodb.net/impulse"
+    MONGODB_CONNECTION_URI: "mongodb://admin:Bitcamp1!@impulse-shard-00-00-0bqxp.mongodb.net:27017,impulse-shard-00-01-0bqxp.mongodb.net:27017,impulse-shard-00-02-0bqxp.mongodb.net:27017/test?ssl=true&replicaSet=impulse-shard-0&authSource=admin"
 };
