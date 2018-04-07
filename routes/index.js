42fbc60f56cbc0469d5d802c3419ace4d5cb717d
Michael <michael.weinberger09@gmail.com>
2018-04-06 23:18:43 -0400

    Added Routes

--- routes/index.js
+++ routes/index.js
@@ -6,5 +6,21 @@ router.get('/', function(req, res, next) {
     return res.render('index');
 });
 
+router.get('/dashboard', function(req, res, next) {
+    return res.render('dashboard');
+});
+
+router.get('/options', function(req, res, next) {
+    return res.render('options');
+});
+
+router.get('/login', function(req, res, next) {
+    return res.render('sign-in');
+});
+
+router.get('/signup', function(req, res, next) {
+    return res.render('sign-up');
+});
+
 
 module.exports = router;
