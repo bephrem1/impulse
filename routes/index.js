4ae81e55bc8ada48c2918e6451e9b310cbd75cfc
Richard Wang <rkwang@umd.edu>
2018-04-07 16:08:57 -0400

    Templating

--- routes/index.js
+++ routes/index.js
@@ -20,8 +20,26 @@ router.get('/dashboard', mid.requiresLogin, function(req, res, next) {
             if (error) {
                 return res.redirect('/');
             } else {
+                let impulseSum = 0;
+                let impulseDollars = 0;
+                user.purchaseData.forEach(element => {
+                    if (element.wasPurchased == false){
+                        impulseSum++;
+                        impulseDollars += element.itemPrice;
+                    }
+                    // // console.log(dateHelpers.epochToDateString(element.purchaseDate));
+                    // element.purchaseDate = 5;
+                    // // console.log("why does this not update");
+                    // element['purchaseDate'] = dateHelpers.epochToDateString(element.purchaseDate);
+                    // element.purchaseDate = "what";
+                    // console.log(element['purchaseDate']);
+                });
+
+
                 // If no error render dashboard and pass user object in model
-                return res.render('dashboard', {user});
+                return res.render('dashboard', {
+                    user, impulseDollars, impulseSum
+                });
             }
         });
 
